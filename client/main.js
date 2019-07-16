dda8b4330e3df768fc146966f62c46b1e2759fad
Andrew <acheung8@uw.edu>
2019-07-15 22:45:46 -0700

    we. have. working. client. side. interaction. now.

--- client/main.js
+++ client/main.js
@@ -1,21 +1,29 @@
 (function() {
 
   window.addEventListener('load', init);
+  let id;
 
   function init() {
-    document.getElementById('get-songs').addEventListener('click', getSongs)
+    document.getElementById('get-songs').addEventListener('click', getArtist)
   }
 
-  function getSongs() {
+  function getArtist() {
     let artistName = document.getElementById('artist-name').value;
     if (artistName.length === 0) {
       console.log('no');
       return;
     } else {
-      fetch('http://localhost:3000/getsongs?artist=' + artistName)
-        .then((data) => data.json())
-        .then(console.log)
+      fetch('http://localhost:3000/getartist?artist=' + artistName)
+        .then(data => data.json())
+        .then(displayArtistData)
         .catch(console.error);
     }
+  }
+
+  function displayArtistData(data) {
+    id = data.id;
+    console.log(id);
+    document.getElementById('artist-pic').src = data.img;
+    document.getElementById('response').innerText = data.name;
   }
 }());
--- server/index.js
+++ server/index.js
@@ -17,13 +17,77 @@ const spotifyApi = new SpotifyWebApi({
 // let me test this "api" locally.
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
+    res.header("Access-Control-Allow-Credentials", 'true');
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
-authenticate();
+// oof these endpoint names could probably use some work.
+app.get('/getartist', function(req, res) {
+  // let's deal with case of no auth later.
+  if (!req.query.artist) {
+    return res.status(400).send({
+      message: 'No artist param passed!'
+    });
+  } else {
+    spotifyApi.searchArtists(req.query.artist)
+      .then((data) => {
+        let chosenArtist = data.body.artists.items[0];
+        let artist = {
+          name: chosenArtist.name,
+          id: chosenArtist.id,
+          img: chosenArtist.images[0].url
+        }
+        return artist;
+      })
+      .then((data) => res.send(data))
+      .catch(console.error);
+  }
+});
 
+app.get('/getsongs', function(req, res) {
+  if (!req.query.artist) {
+    return res.status(400).send({
+      message: 'No artist param passed!'
+    });
+  } else {
+    spotifyApi.getArtistAlbums(req.query.artist, { include_groups: 'album', limit: 50 })
+      .then(data => {
+        return data.body.items;
+      })
+      .then(albums => {
+        let albumObjs = [];
+        albums.forEach(album => {
+          let albumObj = {
+            name: album.name,
+            id: album.id,
+            imgUrl: album.images[0].url,
+            // songs: Array() // lol i like to do [] but i wanted to flex
+          }
+            albumObjs.push(albumObj);
+        });
+        return albumObjs;
+      })
+      .then(cleanse)
+      .then(data => res.send(data))
+      .catch(error);
+  }
+});
+
+function cleanse(items) {
+  for (let i = 0; i < items.length; i++) {
+    for (let j = i + 1; j < items.length; j++) {
+      if (items[i].name === items[j].name) {
+        items.splice(j--, 1);
+      }
+    }
+  }
+  return items;
+}
+
+// TODO: make maybe this synchronous such that other funcs can't
+// fire until successful authentication
 function authenticate() {
   spotifyApi.clientCredentialsGrant()
     .then(data => {
@@ -33,20 +97,14 @@ function authenticate() {
     })
   }
 
-app.get('/getsongs', function(req, res) {
-  // let's deal with case of no auth later.
-  if (!req.query.artist) {
+  // Returns sufficiently ambiguous error message that for whatever reason
+  // doesn't get fired when it should be.
+  function error(err) {
+    console.error(err);
     return res.status(400).send({
-      message: 'No artist returned!'
+      message: 'Aiya! There was an error!'
     });
-  } else {
-     spotifyApi.searchArtists(req.query.artist)
-      .then((data) => {        
-        res.setHeader('Content-Type', 'application/json');
-        res.json(data);
-      })
-      .catch(console.error);
   }
-});
 
-app.listen(process.env.PORT, () => console.log(`Example running on ${process.env.PORT}!`));
+authenticate();
+app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}!`));
