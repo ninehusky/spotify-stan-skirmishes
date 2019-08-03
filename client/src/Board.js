dec040794ac270c44525f0627f1dd38ea292cd44
kevin <acheung8@uw.edu>
2019-08-02 23:33:19 -0700

    I think this works

--- client/src/Board.js
+++ client/src/Board.js
@@ -6,39 +6,108 @@ class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hits: null,
+            artistData: null,
             isLoading: true,
+            choiceMade: false,
         };
     }
     
     componentDidMount() {
-        fetch(URL_BASE + 'getartist?artist=kanye')
+        //   fetch(URL_BASE + 'getartist?artist=kanye')
+        //       .then(response => response.json())
+        //       .then(data => this.setState({
+        //           hits: data,
+        //           isLoading: false,
+        //           hidden: true, // we finna use this later
+        //       }))
+        //       .catch(console.error);
+    }
+
+    artistSearch = (artist) => {
+        // probably should scan to see if string is empty
+        fetch(URL_BASE + 'getartist?artist=' + artist)
             .then(response => response.json())
             .then(data => this.setState({
-                hits: data,
+                artistData: data,
                 isLoading: false,
-                hidden: true, // we finna use this later
             }))
             .catch(console.error);
     }
 
+    makeChoice = (choice) => {
+        this.setState({
+            isLoading: !choice, // if user chose 'yes', we are no longer loading.
+        });
+    }
+
     render() {
         if (this.state.isLoading) {
-            return <h1>Loading...</h1>;
+            console.log(this.state.isLoading);
+            return <SearchCard artistSearch={this.artistSearch}/>
         } else {
             return(
-                <Card artistData={ this.state.hits }/>
+                <ArtistCard artistData={ this.state.artistData }
+                            makeChoice={ this.makeChoice }/>
             );
         }
     }
 }
 
-class Card extends Component {
+class SearchCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: "",
+        }
+    }
+    
+    artistSearch = () => {
+        let artist = this.state.inputValue;
+        this.props.artistSearch(artist);
+    }
+
+    updateInputValue = (evt) => {
+        this.setState({
+            inputValue: evt.target.value,
+        });
+    }
+
+    render() {
+        return(
+            <div className="input-group">
+                <input type="text" placeholder="Artist name"
+                       value={ this.state.inputValue }
+                       onChange={evt => this.updateInputValue(evt)}/>
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary"
+                            type="button"
+                            onClick={ this.artistSearch }>
+                        lets go baby
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+class ArtistCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
             data: this.props.artistData,
+            choice: null,
         }
+    }
+
+    // this should maybe be like handleClick(), maybe i should make that for all objects
+    confirmChoice = (evt) => {
+        let id = evt.target.id;
+        if (id === "yes") { // holy 
+            id = true;
+        } else {
+            id = false;
+        }
+        this.props.makeChoice(id);
     }
 
     render() {
@@ -50,10 +119,16 @@ class Card extends Component {
                     >
                     </img>
                     <p>{ this.state.data.name }</p>
-                    <div className="container">
-                        <p>Is this it?</p>
-                        <button className="btn btn-primary">Yes</button>
-                        <button className="btn btn-secondary">Nope</button>
+                    <p>Is this it?</p>
+                    <div className="container w-75" id="button-holder">
+                        <button id="yes" className="btn btn-primary"
+                                onClick={ this.confirmChoice }>
+                                    Yes
+                        </button>
+                        <button id="no" className="btn btn-secondary"
+                                onClick={ this.confirmChoice }>
+                                Nope
+                        </button>
                     </div>
                 </div>
             </div>
